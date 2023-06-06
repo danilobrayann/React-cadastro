bf1b0f38b77c65c9b4856ec6765bd05fa8556007
DANILO BRAYANN <116122936+danilobrayann@users.noreply.github.com>
2023-06-06 17:30:55 -0300

    feat:atualização de layout e modificação no ipunt dos nomes cadastrados

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,58 @@
-// import React from 'react'
-import People from './assets/pessoas.svg'
-import Arrow from './assets/flecha.svg'
+import React from 'react';
+import People from './assets/pessoas.svg';
+import Arrow from './assets/flecha.svg';
+import lixo from './assets/lixo.svg'
 
-import { Container, H1, Imagem, ContainerIntes, InputLabel, Input, Button, ImgFlecha } from './styles'
+import {
+  Container,
+  H1,
+  Imagem,
+  ContainerIntes,
+  InputLabel,
+  Input,
+  Button,
+  ImgFlecha,
+  User, 
+} from './styles';
 
-// jsx
-const App = () => {
-  // o retorno sempre vai trazer arquivos html
+interface Pessoa {
+  id: number;
+  name: string;
+  age: number;
+}
+//Jsx
+const App: React.FC = () => {
+  const users: Pessoa[] = [
+    { id: Math.random(), name: 'Danilo', age: 26 },
+    { id: Math.random(), name: 'Maria', age: 20 },
+  ];
+
   return (
-
-    <Container >
-      <Imagem src={People} alt='People' />
+    <Container>
+      <Imagem src={People} alt="People" />
       <ContainerIntes>
-        <H1>Olá </H1>
+        <H1>Olá</H1>
         <InputLabel>Nome</InputLabel>
-        <Input placeholder='nome' />
+        <Input placeholder="nome" />
 
         <InputLabel>Idade</InputLabel>
-        <Input placeholder='idade' />
+        <Input placeholder="idade" />
 
-        <Button>Cadastra <ImgFlecha src={Arrow} alt="seta" /></Button>
+        <Button>
+          Cadastra <ImgFlecha src={Arrow} alt="seta" />
+        </Button>
+        <ul>
+          {users.map((user) => (
+            <User key={user.id}>
+              <p>{user.name}</p>  <p>{user.age}</p>
+            <button><img src={lixo} alt="lata de lixo" /></button>
+              
+              </User>
+          ))}
+        </ul>
       </ContainerIntes>
     </Container>
+  );
+};
 
-  )
-
-}
-
-
-export default App
+export default App;
--- src/styles.ts
+++ src/styles.ts
@@ -8,7 +8,7 @@ display:flex;
 flex-direction:column;
 align-items:center;
 gap:40px;
-height:100vh;
+height:100%;
 
 `;
 
@@ -32,7 +32,6 @@ flex-direction:column;
 
 
 export const H1 = styled.h1`
-
 font-style: normal;
 font-weight: 700;
 font-size: 34px;
@@ -91,8 +90,44 @@ display:flex;
 align-items:center;
 justify-content:center;
 gap:20px;
+
+&:hover{
+    opacity:0.8;
+}
+ &:active{
+    opacity:0.5;
+ }
 `;
 
 export const ImgFlecha = styled.img`
+
+`
+export const User = styled.li`
+display:flex;
+justify-content:space-around;
+align-items:center;
+height:50px;
+width:342;
+background: rgba(255, 255, 255, 0.25);
+box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+border-radius: 14px;
+border:none;
+outline:none;
+margin-bottom:25px;
+margin-top:25px;
+
+p{
+font-style: normal;
+font-weight: 400;
+font-size: 20px;
+line-height: 28px;
+color: #FFFFFF;
+}
+
+button{
+    background:none;
+    border:none;
+    cursor: pointer;
+}
 
 `
